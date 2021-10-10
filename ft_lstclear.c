79a06c9ff9f09abcace43458f5045ab38adbc387
Saim Ali <sali@lab3r6s8.42abudhabi.ae>
2021-10-10 21:23:49 +0400

    almost donez

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sali <sali@student.42abudhabi.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 18:42:35 by sali              #+#    #+#             */
+/*   Updated: 2021/10/10 19:31:35 by sali             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{ 
+	t_list	*ptr;
+
+	ptr = *lst;
+	while(ptr != NULL)
+	{
+		ptr = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = ptr;
+	}
+	lst = NULL;
+}
